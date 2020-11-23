08b60e973cb68301cb99a752933986ffb045ccd7
Mirza Tanzim Sami <43340055+MTSami@users.noreply.github.com>
2020-11-23 15:50:07 -0600

    Fixed Tokenizer bug

--- run_language_modeling.py
+++ run_language_modeling.py
@@ -248,6 +248,8 @@ def main():
         trainer.save_model()
         # For convenience, we also re-save the tokenizer to the same directory,
         # so that you can share your model easily on huggingface.co/models =)
+        
+        #Fixed Tokenizer bug
         tokenizer.save_pretrained(training_args.output_dir)
 
     # Evaluation
